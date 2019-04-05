47e14c5c2ee4b6d5fb70b5e2cc437505b927a9da
Tomas Coufal <tcoufal@redhat.com>
2019-04-05 19:35:01 +0200

    Training foundation

---
+++ src/utils/losses.py
@@ -0,0 +1,16 @@
+import keras.backend as k
+
+def margin_loss(true, predicted):
+    """Margin loss.
+
+    Args:
+        true ([None, markers_count]): Label
+        predicted ([None, capsules_count]): Predicted label
+
+    Returns:
+        Scalar loss
+    """
+    loss = true * k.maximum(.0, .9 - predicted) + \
+           .5 * (1 - true) * k.square(.0, predicted - .1)
+
+    return k.mean(k.sum(loss, 1))
--- src/utils/network.py
+++ src/utils/network.py
@@ -1,6 +1,7 @@
 import numpy as np
 from keras import models, layers, callbacks, optimizers
 import layers as custom_layers
+from losses import margin_loss
 
 class CapsNet:
     """Capsule neural network for Face Recognition.
@@ -22,55 +23,61 @@ class CapsNet:
         # Input layer
         encoder_input = layers.Input(shape=input_shape)
         # Encoder
-        encoder_output = model.Sequential([
-            layers.Conv2D(
-                filters=256,
-                kernel_size=9,
-                strides=1,
-                padding='valid',
-                activation='relu',
-                name='encoder_conv'
-            ),
-            custom_layers.PrimaryCapsule(
-                capsule_dim=8,
-                channels_count=32,
-                kernel_size=9,
-                strides=2,
-                padding='valid',
-                name='encoder_primary_caps'
-            ),
-            custom_layers.Capsule(
-                capsule_count=markers_count,
-                capsule_dim=16,
-                routing_iters=3,
-                name='encoder_features_caps'
-            )
-        ])
+        encoder = models.Sequential(
+            name='encoder',
+            layers=[
+                layers.Conv2D(
+                    filters=256,
+                    kernel_size=9,
+                    strides=1,
+                    padding='valid',
+                    activation='relu',
+                    name='encoder_conv'
+                ),
+                custom_layers.PrimaryCapsule(
+                    capsule_dim=8,
+                    channels_count=32,
+                    kernel_size=9,
+                    strides=2,
+                    padding='valid',
+                    name='encoder_primary_caps'
+                ),
+                custom_layers.Capsule(
+                    capsule_count=markers_count,
+                    capsule_dim=16,
+                    routing_iters=3,
+                    name='encoder_features_caps'
+                )
+            ]
+        )
 
         # Decoder
         decoder_input = layers.Input(shape=markers_count)(encoder)
 
-        decoder = models.Sequential([
-            layers.Dense(
-                units=512,
-                activation='relu',
-                input_dim=16*markers_count,
-                name='decoder_dense_1'
-            ),layers.Dense(
-                units=1024,
-                activation='relu',
-                name='decoder_dense_2'
-            ),
-            layers.Dense(
-                units=np.prod(input_shape),
-                activation='sigmoid',
-                name='decoder_dense_3'
-            ),
-            layers.Reshape(
-                target_shape=input_shape,
-                name='decoder_reshape'
-            )
-        ])
+        decoder = models.Sequential(
+            name='decoder',
+            layers=[
+                layers.Dense(
+                    units=512,
+                    activation='relu',
+                    input_dim=16*markers_count,
+                    name='decoder_dense_1'
+                ),layers.Dense(
+                    units=1024,
+                    activation='relu',
+                    name='decoder_dense_2'
+                ),
+                layers.Dense(
+                    units=np.prod(input_shape),
+                    activation='sigmoid',
+                    name='decoder_dense_3'
+                ),
+                layers.Reshape(
+                    target_shape=input_shape,
+                    name='decoder_reshape'
+                )
+            ]
+        )
 
         decoder_train_output = decoder()
         decoder_inference_output = decoder()
@@ -79,17 +86,17 @@ class CapsNet:
         self.models = dict(
             train=models.Model(
                 [encoder_input, decoder_input],
-                [encoder_output(encoder_input), decoder_train_output()]
+                [encoder(encoder_input), decoder_train_output()]
             ),
             inference=models.Model(
                 [encoder_input],
-                [encoder_output(encoder_input), decoder_inference_output()]
+                [encoder(encoder_input), decoder_inference_output()]
             )
         )
 
 
     def train(self, data, batch_size=100, epochs=50,
-              lr=0.001, lr_decay=0.4,
+              lr=0.001, lr_decay=0.4, decoder_loss_weight=.4,
               save_dir=None):
         """Train the network.
 
@@ -103,7 +110,7 @@ class CapsNet:
         (x_train, y_train), (x_test, y_test) = data
 
         # Callback
-        callbacks = [
+        cb = [
             callbacks.CSVLogger(f'{save_dir}/log.csv'),
             callbacks.LearningRateScheduler(lambda e: lr * (lr_decay ** e)),
             callbacks.ModelCheckpoint(
@@ -115,11 +122,22 @@ class CapsNet:
             ),
         ]
 
-
         self.models['train'].compile(
             optimizer=optimizers.Adam(lr=lr),
-            loss=[loss, ]
+            loss=[loss, 'mse'],
+            loss_weights=[1., decoder_loss_weight],
+            metrics={'capsnet': 'accuracy'}
         )
+
+        self.models['train'].fit_generator(
+            generator=None,
+            steps_per_epoch=int(y_train.shape[0] / batch_size),
+            epochs=epochs,
+            validation_data=[[x_test, y_test], [y_test, x_test]],
+            callbacks=cb
+        )
+
+        self.models['train'].save_weights(f'{save_dir}/model.h5')
 
     def inference(self, data):
         pass
